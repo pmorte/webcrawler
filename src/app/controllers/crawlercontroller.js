8c9513caba1eee64802730379cd99d6a64b3690b
Paulo Boa Morte <paulo.bmorte@gmail.com>
2023-11-24 00:26:47 -0300

    add images

--- src/app/controllers/crawlercontroller.js
+++ src/app/controllers/crawlercontroller.js
@@ -12,10 +12,23 @@ class CrawlerControler{
         for (let i = 0; i < itens.length; i++) {
             await page.goto(url+itens[i]);
             preco[i] = await page.evaluate(()=>{
+                var desconto;
+                var indexMenorPreco;
+                try {
+                    desconto = document.querySelectorAll('.discount.label')[0].innerHTML;
+                    indexMenorPreco = 2;
+                } catch (error) {
+                    desconto = "-";
+                    indexMenorPreco = 1;
+                }
                 return{
                     "nome": "",
                     "atual": document.querySelectorAll('.price-inner.numeric')[0].innerHTML,
-                    "menor": document.querySelectorAll('.price-inner.numeric')[2].innerHTML
+                    "menor": document.querySelectorAll('.price-inner.numeric')[indexMenorPreco].innerHTML,
+                    "img_loja": document.querySelectorAll(".d-flex.flex-align-center.shop-link")[0]
+                    .getElementsByTagName("img")[0].currentSrc,
+                    "desconto": desconto,
+                    "img_jogo": document.getElementsByClassName("image-game")[0].src
                 }
             });
             preco[i].nome = itens[i].replace(/-/g," ");
--- src/app/statics/crawler.js
+++ src/app/statics/crawler.js
@@ -7,14 +7,18 @@ async function get_preco() {
     for (let i = 0; i < response.data.length; i++) {
         var novaLinha = tbValue.insertRow();
 
-        var loja = novaLinha.insertCell(0);
-        var Nome = novaLinha.insertCell(1);
-        var atual = novaLinha.insertCell(2);
-        var menor = novaLinha.insertCell(3);
+        var jogo = novaLinha.insertCell(0);
+        var loja = novaLinha.insertCell(1);
+        var nome = novaLinha.insertCell(2);
+        var atual = novaLinha.insertCell(3);
+        var desconto = novaLinha.insertCell(4);
+        var menor = novaLinha.insertCell(5);
 
-        loja.innerHTML = "loja";
-        Nome.innerHTML = response.data[i].nome;
+        jogo.innerHTML = `<img src='${response.data[i].img_jogo}' style='max-width:90px; max-height:55px'>`;
+        loja.innerHTML = `<img src='${response.data[i].img_loja}' style='max-width:90px; max-height:35px'>`;
+        nome.innerHTML = response.data[i].nome;
         atual.innerHTML = response.data[i].atual;
+        desconto.innerHTML = response.data[i].desconto;
         menor.innerHTML = response.data[i].menor;
     }
 }
