16ac49e44abf60dba859ae4873321b3cd6603068
Paulo Boa Morte <paulo.bmorte@gmail.com>
2023-12-04 10:14:07 -0300

    add header

--- src/app/app.js
+++ src/app/app.js
@@ -12,6 +12,7 @@ app.use(express.static(path.join(__dirname, 'statics')));
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
 
